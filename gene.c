e33efcb2e7cb88b46a402e04e86e5fb7595a7bb4
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2024-10-27 20:58:55 +0900

    add: gene bool support

--- gene.c
+++ gene.c
@@ -13,6 +13,7 @@ overloadable void printany(int x) { printf("%d", x); }
 overloadable void printany(size_t x) { printf("%zu", x); }
 overloadable void printany(double x) { printf("%lf", x); }
 overloadable void printany(char x) { printf("%c", x); }
+overloadable void printany(bool x) { printf(ifx(x) "true" elsex "false"); }
 overloadable void printany(char *x) { printf("%s", x); }
 overloadable void printany(void *x) { printf("%p", x); }
 
@@ -20,6 +21,7 @@ overloadable bool eq(int x, int y) { return x == y; }
 overloadable bool eq(size_t x, size_t y) { return x == y; }
 overloadable bool eq(double x, double y) { return double_eq(x, y); }
 overloadable bool eq(char x, char y) { return complex_eq(x, y); }
+overloadable bool eq(bool x, bool y) { return x == y; }
 overloadable bool eq(char *x, char *y) { return !strcmp(x, y); }
 overloadable bool eq(void *x, void *y) { return x == y; }
 
