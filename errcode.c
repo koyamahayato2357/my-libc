a57ee910aa38754ac8a3f48b80378623af4f5179
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2024-10-27 12:49:57 +0900

    add: codetomsg

--- errcode.c
+++ errcode.c
@@ -8,6 +8,10 @@ const char *codetomsg(int code) {
   switch (code) {
   case ERR_SUCCESS:
     return "success";
+  case ERR_ALLOC_FAILED:
+    return "allocation failure";
+  case ERR_REACHED_UNREACHABLE:
+    return "reached unreachable";
   default:
     return "";
   }
