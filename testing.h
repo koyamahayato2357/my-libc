a81b079d8963dd0fb54f6c66f49ce7ac6c381236
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2024-10-28 15:40:42 +0900

    add: test_table

--- testing.h
+++ testing.h
@@ -15,6 +15,7 @@ extern int TESTING_H_fail;
     int TESTING_H_COL = 3 - (strlen(#name) + 3) / 8;                           \
     jmp_buf jb [[maybe_unused]];                                               \
     printf("\033[34mTesting\033[0m " #name "...");                             \
+    fflush(stdout);                                                            \
     for (int TESTING_H_i = 0; TESTING_H_i < TESTING_H_COL; TESTING_H_i++)      \
       putchar('\t');                                                           \
     printf("\033[2m=> ");                                                      \
@@ -29,6 +30,42 @@ extern int TESTING_H_fail;
     TESTING_H_success++;                                                       \
   }                                                                            \
   void TESTING_H_tester##name(jmp_buf jb [[maybe_unused]])
+#define EXPAND(x) x
+#define GET_MACRO(_1, _2, _3, _4, _5, NAME, ...) NAME
+#define FOR_EACH_1(macro, a) macro(a)
+#define FOR_EACH_2(macro, a, ...)                                              \
+  macro(a), EXPAND(FOR_EACH_1(macro, __VA_ARGS__))
+#define FOR_EACH_3(macro, a, ...)                                              \
+  macro(a), EXPAND(FOR_EACH_2(macro, __VA_ARGS__))
+#define FOR_EACH_4(macro, a, ...)                                              \
+  macro(a), EXPAND(FOR_EACH_3(macro, __VA_ARGS__))
+#define FOR_EACH(...)                                                          \
+  GET_MACRO(__VA_ARGS__, FOR_EACH_4, FOR_EACH_3, FOR_EACH_2, FOR_EACH_1)       \
+  (__VA_ARGS__)
+#define MEMBER_NAME(member) t->member
+#define MEMBER_NAMES(...) FOR_EACH(MEMBER_NAME, __VA_ARGS__)
+#define test_table(name, fn, datast, d, ...)                                   \
+  typedef datast ds##name;                                                     \
+  __attribute__((constructor)) void TESTING_H_tabletester##name() {            \
+    ds##name *data = d;                                                        \
+    int TESTING_H_COL = 3 - (strlen(#name) + 3) / 8;                           \
+    printf("\033[34mTesting\033[0m " #name "...");                             \
+    fflush(stdout);                                                            \
+    for (int TESTING_H_i = 0; TESTING_H_i < TESTING_H_COL; TESTING_H_i++)      \
+      putchar('\t');                                                           \
+    printf("\033[2m=> ");                                                      \
+    for (size_t i = 0; i < sizeof(d) / sizeof(d[0]); i++) {                    \
+      ds##name *t = &data[i];                                                  \
+      if (fn(MEMBER_NAMES(__VA_ARGS__)) != t->result) {                        \
+        printf("Failed at record %zu ", i);                                    \
+        puts("\033[31m[NG]\033[0m");                                           \
+        TESTING_H_fail++;                                                      \
+        return;                                                                \
+      }                                                                        \
+    }                                                                          \
+    puts("\033[32m[OK]\033[0m");                                               \
+    TESTING_H_success++;                                                       \
+  }
 
 #define main main_
 #else
