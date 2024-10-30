474ce75dfe0423e299cce4ddc4630802fe511e6c
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2024-10-30 17:43:53 +0900

    chore

--- gene.h
+++ gene.h
@@ -7,7 +7,7 @@
   bool eq(T, T) overloadable;
 
 #define APPLY_PRIMITIVE_TYPES(M) M(int) M(size_t) M(double) M(char) M(bool)
-#define APPLY_POINTER_TYPES(M)                                                   \
+#define APPLY_POINTER_TYPES(M)                                                 \
   M(int *) M(size_t *) M(double *) M(char *) M(bool *) M(void *)
 
 APPLY_PRIMITIVE_TYPES(DEF_GEN)
