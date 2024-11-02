47bf434610332a84977bfd65aec6e3f42763ca96
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2024-11-02 17:46:09 +0900

    change: log fprintf

--- log.h
+++ log.h
@@ -17,37 +17,37 @@ enum {
 #endif
 
 #if LOG_LEVEL <= LOG_LEVEL_TRACE
-#define LOG_TRACE(...) printf(ESTHN "[TRACE]" ESCLR __VA_ARGS__)
+#define LOG_TRACE(...) fprintf(stderr, ESTHN "[TRACE] " ESCLR __VA_ARGS__)
 #else
 #define LOG_TRACE(...)
 #endif
 
 #if LOG_LEVEL <= LOG_LEVEL_DEBUG
-#define LOG_DEBUG(...) printf(ESTHN "[DEBUG]" ESCLR __VA_ARGS__)
+#define LOG_DEBUG(...) fprintf(stderr, ESTHN "[DEBUG] " ESCLR __VA_ARGS__)
 #else
 #define LOG_DEBUG(...)
 #endif
 
 #if LOG_LEVEL <= LOG_LEVEL_INFO
-#define LOG_INFO(...) printf(ESTHN "[INFO]" ESCLR __VA_ARGS__)
+#define LOG_INFO(...) fprintf(stderr, ESTHN "[INFO] " ESCLR __VA_ARGS__)
 #else
 #define LOG_INFO(...)
 #endif
 
 #if LOG_LEVEL <= LOG_LEVEL_WARN
-#define LOG_WARN(...) printf(ESTHN "[WARN]" ESCLR __VA_ARGS__)
+#define LOG_WARN(...) fprintf(stderr, ESTHN "[WARN] " ESCLR __VA_ARGS__)
 #else
 #define LOG_WARN(...)
 #endif
 
 #if LOG_LEVEL <= LOG_LEVEL_ERROR
-#define LOG_ERROR(...) printf(ESTHN "[ERROR]" ESCLR __VA_ARGS__)
+#define LOG_ERROR(...) fprintf(stderr, ESTHN "[ERROR] " ESCLR __VA_ARGS__)
 #else
 #define LOG_ERROR(...)
 #endif
 
 #if LOG_LEVEL <= LOG_LEVEL_CRITICAL
-#define LOG_CRITICAL(...) printf(ESTHN "[CRITICAL]" ESCLR __VA_ARGS__)
+#define LOG_CRITICAL(...) fprintf(stderr, ESTHN "[CRITICAL] " ESCLR __VA_ARGS__)
 #define LOG_PRINTANY(x) printany(x)
 #else
 #define LOG_CRITICAL(...)
