2d76c9ecae8ed42f6bbcb92689f96e26b27462fa
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2024-11-10 10:21:51 +0900

    chore

--- parallel.h
+++ parallel.h
@@ -5,14 +5,14 @@
 #define async(fn, arg)                                                         \
   ({                                                                           \
     pthread_t pt;                                                              \
-    if (pthread_create(&pt, nullptr, fn, (void *)arg) != 0)                    \
+    if (pthread_create(&pt, nullptr, fn, (void *)arg))                         \
       panic(ERR_THREAD_CREATE);                                                \
     pt;                                                                        \
   })
 #define await(pt)                                                              \
   ({                                                                           \
     void *ret;                                                                 \
-    if (pthread_join(pt, &ret) != 0)                                           \
+    if (pthread_join(pt, &ret))                                                \
       panic(ERR_THREAD_JOIN);                                                  \
     ret;                                                                       \
   })
