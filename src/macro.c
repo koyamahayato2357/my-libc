6c2aa380c747e1ec1c30ed9edbfd0129590c6982
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2025-03-05 17:11:56 +0900

    add macro.c test

--- src/macro.c
+++ src/macro.c
@@ -1,7 +1,16 @@
 #include "macro.h"
 #include "testing.h"
 
-test(macro) {
+test (macro) {
   expecteq("0", TOSTR(BOOL(0)));
   expecteq("1", TOSTR(BOOL(100)));
 }
+
+test_table(
+  if, , (int, int),
+  {
+    {0, IF(10)(0, 10)},
+    {1, IF(1)(IF(1)(1, 5))},
+    {1, IF(0)(IF(1)(0, 1), 1)},
+}
+)
