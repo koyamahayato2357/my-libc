4471f0c14d99f06acdce040034d004d8f068f0f3
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2025-03-07 10:48:58 +0900

    enhance: double_eq

--- src/gene.c
+++ src/gene.c
@@ -1,71 +1,94 @@
 #include "gene.h"
 #include "exproriented.h"
+#include "testing.h"
 #include <stdio.h>
 #include <string.h>
 #include <tgmath.h>
 
-#define EPSILON 1e-5
+constexpr double eps = 1e-5;
 
 static bool double_eq(double a, double b) {
-  return fabs(a - b) < EPSILON;
+  if (fabs(b) < eps) return fabs(a) < eps; // prevent 0-div when b is near 0
+  if (a < 0 != b < 0) return false;        // mis signed
+  return fabs(a / b - 1.0) < eps;          // cmp based on ratios
 }
 static bool complex_eq(double complex a, double complex b) {
-  return fabs(creal(a - b)) < EPSILON && fabs(cimag(a - b)) < EPSILON;
+  return double_eq(creal(a), creal(b)) && double_eq(cimag(a), cimag(b));
 }
 
-overloadable void printany(int x) {
+test_table(
+  double_eq, double_eq, (bool, double, double),
+  {
+    { true,      1.0,          1.0},
+    { true,    1e-10,            0},
+    { true,    1e100, 1e100 + 1e10},
+    {false,        0,            1},
+    {false,      NAN,          NAN},
+    {false,      NAN,         1e10},
+    {false, INFINITY,     INFINITY},
+    {false, INFINITY,        1e100},
+}
+)
+test_table(
+  complex_eq, complex_eq, (bool, double complex, double complex),
+  {
+    { true,                0,          0},
+    { true,           1 + 3i, 1.0 + 3.0i},
+    { true, 1.0 + 1e-10 + 3i,     1 + 3i},
+    {false,                0,          1},
+    {false,           3 + 5i,    -1 + 3i},
+}
+)
+
+#pragma clang attribute push(overloadable, apply_to = function)
+void printany(int x) {
   printf("%d", x);
 }
-overloadable void printany(size_t x) {
+void printany(size_t x) {
   printf("%zu", x);
 }
-overloadable void printany(double x) {
+void printany(double x) {
   printf("%lf", x);
 }
-overloadable void printany(char x) {
+void printany(char x) {
   printf("%c", x);
 }
-overloadable void printany(bool x) {
+void printany(bool x) {
   printf($if(x) "`true`" $else "`false`");
 }
-overloadable void printany(char *x) {
+void printany(char *x) {
   printf("%s", x);
 }
-overloadable void printany(void *x) {
+void printany(void *x) {
   printf("%p", x);
 }
 
-overloadable bool eq(int x, int y) {
-  return x == y;
-}
-overloadable bool eq(size_t x, size_t y) {
-  return x == y;
-}
-overloadable bool eq(double x, double y) {
+#define CMP_DIRECTLY(T) \
+  bool eq(T x, T y) { \
+    return x == y; \
+  }
+CMP_DIRECTLY(int)
+CMP_DIRECTLY(size_t)
+CMP_DIRECTLY(char)
+CMP_DIRECTLY(bool)
+CMP_DIRECTLY(void *)
+bool eq(double x, double y) {
   return double_eq(x, y);
 }
-overloadable bool eq(char x, char y) {
-  return complex_eq(x, y);
-}
-overloadable bool eq(bool x, bool y) {
-  return x == y;
-}
-overloadable bool eq(char *x, char *y) {
+bool eq(char *x, char *y) {
   return !strcmp(x, y);
-}
-overloadable bool eq(void *x, void *y) {
-  return x == y;
 }
 
 #define DEF_FNS(T) \
-  overloadable void printanyln(T x) { \
+  void printanyln(T x) { \
     printany(x); \
     putchar('\n'); \
   }
 TMAP(DEF_FNS)
 #define DEF_PFNS(T) \
-  overloadable void printanyln(T *x) { \
+  void printanyln(T *x) { \
     printany((void *)x); \
     putchar('\n'); \
   }
 TMAP(DEF_PFNS)
+#pragma clang attribute pop
